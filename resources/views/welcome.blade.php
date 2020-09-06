0a2b2154e557dc257a76ead72f0abd5f503b701e
Alvaro Castillo <alvarolucascc96@gmail.com>
2020-09-05 21:52:26 -0300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-md-8 justify-content-center">
+            <div class="card border-secondary shadow">
+                <div class="card-header h2 bg-tertiary">
+                    <div class="row">
+                        <div class="col">
+                            CAPACITACIÓN TRABAJOS EN ALTURA FÍSICA
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form >
+                        <label for="rut" name="rut"> Ingrese su RUT (Formato 12346789-K): </label>
+                        <input type="text" name="rut" id="rut" class="form-control"><br>
+
+                        <label for="nombre" name="nombre"> Ingrese su nombre completo: </label>
+                        <input type="text" name="nombre" id="nombre" class="form-control"><br>
+
+                        <label for="nombre" name="nombre"> Ingrese SAP: </label>
+                        <input type="text" name="nombre" id="nombre" class="form-control">
+
+                        <div class="form-group mt-4 text-center">
+                            <button type="submit" class="btn btn-primary">Continuar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
